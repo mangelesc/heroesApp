3b7d951a52caa349943022335ea1427105a9e231
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-11-12 19:19:19 +0100

    Rutas hijas Heroes Module

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,11 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
 const routes: Routes = [
   {
     path:'',
-    component: LayoutPageComponent
+    component: LayoutPageComponent,
+    // Load children -> lazy load
+    // Children -> NO lazy load (Este módulo ya se está cargando en lazy load en el app.module)
+    children: [
+      {
+        path: 'new-hero', 
+        component: NewPageComponent
+      }, 
+      {
+        path: 'search', 
+        component: SearchPageComponent
+      },
+      {
+        path: 'edit/:id', 
+        component: NewPageComponent
+      },
+      {
+        path: 'list', 
+        component: ListPageComponent
+      },
+      {
+        // Cuidado donde colocamos :id, si lo ponemos al principio no entrará en los demás
+        path: ':id', 
+        component: HeroPageComponent
+      },
+      {
+        path: '**', 
+        redirectTo: 'list'
+      },
+    ]
   }
 ];
 
