bcad74a9005b480114af724156904be1ffe496d4
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-11-12 19:07:46 +0100

    Paths definidos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    // Lo cargamos mediante LazyLoad 
+    loadChildren: () => import('./auth/auth.module').then ( m => m.AuthModule),
+  },
+  {
+    path: 'heroes',
+    // Lo cargamos mediante LazyLoad 
+    loadChildren: () => import('./heroes/heroes.module').then ( m => m.HeroesModule),
+  }, 
+  {
+    path: '404', 
+    component: Error404PageComponent,
+  },
+  {
+    // Path por defecto
+    path: '',
+    redirectTo: 'heroes',
+    // para que el path sea exactamente '' y las demas rutas no pasen por aquí 
+    pathMatch: 'full'
+  }, 
+  {
+    // Cualquier otro path
+    path: '**', 
+    redirectTo: '404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
