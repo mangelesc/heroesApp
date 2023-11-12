372a1a226f9293d37c742966e5d86098025997c9
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-11-12 19:22:58 +0100

    Rutas hijas Auth Module

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutPageComponent,
+    // Load children -> lazy load
+    // Children -> NO lazy load (Este módulo ya se está cargando en lazy load en el app.module)
+    children: [
+      {
+        path: 'login', 
+        component: LoginPageComponent
+      }, 
+      {
+        path: 'signup', 
+        component: RegisterPageComponent
+      },
+      {
+        path: '**', 
+        redirectTo: 'login'
+      },
+    ]
   }
 ];
 
