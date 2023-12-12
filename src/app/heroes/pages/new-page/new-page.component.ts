a0c86ae521c8718a1c8fae2514960fb8bb9f9d90
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-12-12 20:32:09 +0100

    Crear heroes

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -7,9 +10,60 @@ import { Component } from '@angular/core';
   ]
 })
 export class NewPageComponent {
+
+
+  // Formulario reactivo
+
+  // Cada elemento de nuestro formulario tiene que estár contenido dentro de un elemento html padre
+
+  // El valor entre () es el valor inicial. 
+  heroForm = new FormGroup({
+    id:         new FormControl<string>(''),
+    superhero:  new FormControl<string>('', { nonNullable: true } ),
+    publisher:  new FormControl<Publisher>( Publisher.DCComics ),
+    first_appearance: new FormControl<string>(''),
+    alter_ego:  new FormControl<string>(''),
+    characters: new FormControl<string>(''),
+    alt_img:    new FormControl(''),
+  });
+
   public publishers = [
     {id: 'DC Comics', desc:'CD - Comics'}, 
     {id: 'Marvel Comics', desc:'Marvel - Comics'}
   ];
+
+  constructor( private HeroesService: HeroesService ) {}
+
+  get currentHero():Hero {
+    const hero = this.heroForm.value as Hero; 
+
+    return hero;
+  }
+
+  onSubmit():void {
+    // console.log({
+    //   formIsValid: this.heroForm.valid,
+    //   value: this.heroForm.value,
+    // })
+    
+    if ( this.heroForm.invalid ) return; 
+
+    if ( this.currentHero.id ){
+
+      // Al ser un observable me tengo que suscribir, sino NO se dispara
+      this.HeroesService.updateHero( this.currentHero )
+        .subscribe( hero => {
+
+        } )
+      return;
+    }
+
+    this.HeroesService.addHero( this.currentHero )
+      .subscribe( hero => {
+
+      } )
+    // this.HeroesService.updateHero( this.heroForm.value );
+
+  }
 
 }
